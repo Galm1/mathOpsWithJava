61073dc7dc01df68a14a69838436904f7cb270a9
EliGarcia <garciaelisha93@yahoo.com>
2017-09-11 14:20:18 -0500

    it is working but i am not using the showSum method. need to figure that out

--- src/com/theironyard/installparty/Main.java
+++ src/com/theironyard/installparty/Main.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+        Scanner in = new Scanner(System.in);
+        double[] nums = new double[2];
+
+        System.out.println("Enter "+nums.length+" numbers: ");
+        for(int i = 0; i < nums.length; i++)
+            nums[i] = in.nextDouble();
+
+           double operand1 = nums[0];
+           double operand2 = nums[1];
+
+           double sum = operand1 + operand2;
+           double difference = operand1 - operand2;
+           double division = operand1 / operand2;
+           double multiplication = operand1 * operand2;
+           double remainder = operand1 % operand2;
+
+        System.out.println("the sum is "+sum + ", " + "the difference is "+difference + ", " + "the quotient is "+division + ", " + "the product is "+multiplication + ", " + "and the remainder is "+remainder);
     }
-}
+
+//    public class void showResults(double sum, double difference, double division, double multiplication, double remainder) {
+//        System.out.println(sum + " ," + difference + " ," + division + " ," + multiplication + " ," + remainder);
+//
+//    }
+    }
+
